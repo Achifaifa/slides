631e5382f29f72a47f6a061adda2e1bac08135c4
Yuri Numerov <achi@hush.ai>
2015-07-26 12:18:44 +0200

    updated tel demo

--- EE23/demos/01-telefonos/creartelefonos.py
+++ EE23/demos/01-telefonos/creartelefonos.py
@@ -3,4 +3,4 @@
 import random
 
 with open("./telefonos","w+") as telefonos:
-  for i in xrange(10000000): telefonos.write("Nombre%08i - Apellido%08i - %i\n"%(i+1,i+1,random.randint(900000000,999999999)))
+  for i in xrange(10000): telefonos.write("Nombre%08i - Apellido%08i - %i\n"%(i+1,i+1,random.randint(900000000,999999999)))
--- EE23/demos/01-telefonos/filtrartel.py
+++ EE23/demos/01-telefonos/filtrartel.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
 with open("./telefonos","r") as tel:
-  with open("./telefonos_alava","w+") as tel_out:
-    for line in tel:
-      if line.split(" - ")[-1].startswith("945"): tel_out.write(line)
+  with open("./telefonos_alava","w+") as tel_al:
+    with open("./telefonos_gipu","w+") as tel_gip:
+      with open("./telefonos_biz","w+") as tel_biz:
+        for line in tel:
+          if line.split(" - ")[-1].startswith("945"): tel_al.write(line)
+          if line.split(" - ")[-1].startswith("944"): tel_biz.write(line)
+          if line.split(" - ")[-1].startswith("943"): tel_gip.write(line)
+
