aa16bf395748716d86b724022bb36017b2da3162
Yuri Numerov <achi@hush.ai>
2015-11-28 17:30:11 +0100

    update

--- makegames/code/tetrees/tetrees.py
+++ makegames/code/tetrees/tetrees.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import os, select, sys, termios
+import os, select, sys, termios, time
 
 world=[["." for i in range(10)] for i in range(22)]
 pieces=[["#","#","#","#"],
@@ -9,6 +9,17 @@ pieces=[["#","#","#","#"],
         [["#",".","."],["#","#","#"]],
         [[".",".","#"],["#","#","#"]],
         [["#","#"],["#","#"]]]
+keys={"d":"c","l":"z","r":"v","rot":" "}
+timem={"timepool":0,"previoustime":0}
+
+def loopmanage():
+  """
+  Internally manages the main game loop cycles 
+  """
+
+  timem["timepool"]=0 if timem["timepool"]*1000>=300 else timem["timepool"]+time.time()-timem["previoustime]"
+  timem["previoustime]"=time.time()
+  return not timem["timepool"]
 
 def pressed():
   """
@@ -27,3 +38,6 @@ def pressed():
   finally:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
     return c
+
+while 1:
+  os.system('clear')
